b8b05173afab69506ea044841529f5839bc13884
Sendi John <johnsendi727@gmail.com>
2025-06-07 09:17:32 +0100

    feat: implement decleanup network roadmap

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import TokenizeImpactSection from "@/components/HowToTokonizeImpact/TokenizeImpactSection"
+import NetworkRoadmapSection from "@/components/NetworkRoadmapSection/NetworkRoadmapSection"
 
 const page = () => {
   return (
     <div>
+       <NetworkRoadmapSection/>
       <TokenizeImpactSection/>
     </div>
   )
