89599ba327074f5d5e26c3d0e9bda724a96629bf
JamesVictor-o <victorjames40v@gmail.com>
2025-06-17 23:09:12 +0100

    fixing changes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
   basePath: isProd ? '/decleanup-landingpage' : '',
-  assetPrefix: isProd ? '/decleanup-landingpage/' : '',
   images: {
     unoptimized: true,
   },
